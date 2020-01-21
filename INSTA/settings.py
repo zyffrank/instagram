de0cd3080bb742bf2ce3afab27252f2e298d2856
zhangyunfan <frankyunfanzhang@gmail.com>
2020-01-21 16:16:20 -0500

    prep for deloyment

--- INSTA/settings.py
+++ INSTA/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mqc4m*=p0nzys%t=!zlp#1^&^0jxjkz0nc@o#2*65yw7t2@=p-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'Insta1',
     'imagekit'
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn INSTA.wsgi --log-file -
